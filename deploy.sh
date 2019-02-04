fe7cc8a8aaac6b21aac43e37886dcaf638613fc4
rsergievsky <rsergievsky@yandex.ru>
2019-02-04 23:31:21 +0300

    deploy at 1549312281

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 git add .
 git commit -a -m "deploy at `date +%s`"
 git push origin master
-ssh dosta 'cd /home/hopacha/dostaSeaBattle/ && git pull origin master && npm install && pm2 restart dosta'
+ssh dosta 'cd /home/hopacha/dosta/ && git pull origin master && npm install && pm2 restart dosta'

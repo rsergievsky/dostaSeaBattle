a1358a831b490acfeb734ecabbef5ef2dfffdd67
rsergievsky <rsergievsky@yandex.ru>
2019-02-07 17:16:15 +0300

    deploy at 1549548975

--- modules/env.js
+++ modules/env.js
@@ -15,7 +15,7 @@ module.exports = {
   players: {},
   getAnswer: {
     violation: (id) => {
-      console.log(this);
+      console.log(this.module);
       return `${this.players[id].name}, у нас было условие — подписаться на сообщество и поставить лайк.`;
     },
     miss: (id) => {

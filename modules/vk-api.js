82c6274e78412bf9541e7d6c42da6a9ed44ba331
rsergievsky <rsergievsky@yandex.ru>
2019-02-07 14:54:50 +0300

    deploy at 1549540490

--- modules/vk-api.js
+++ modules/vk-api.js
@@ -55,10 +55,11 @@ module.exports = {
   checkPlayer: async function(user_id) {
     const {response:isMember} = JSON.parse(await rp.get(`https://api.vk.com/method/groups.isMember?group_id=${env.groupID}&user_id=${user_id}&access_token=${cfg.tokens.group}&v=5.92`));
     let likedList = [];
-    getLikes: for (const i = 0; i < 10; i++) {
+    getLikes: for (let i = 0; i < 10; i++) {
       const {response} = JSON.parse(await rp.get(`https://api.vk.com/method/likes.getList?type=post&owner_id=${-env.groupID}&item_id=${env.postID}&count=1000&offset=${i*1000}&access_token=${cfg.tokens.users[env.tokenIndex]}&v=5.92`));
       if (response.items.length === 0) break getLikes;
       likedList.push(response.items);
+      if (response.items.length < 999) break getLikes;
     }
     const isLiked = likedList.includes(user_id);
     console.log(likedList, isMember, isLiked);

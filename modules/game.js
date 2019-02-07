a10191a5b30266e8ed4fc8f758cb37a834d1eb52
rsergievsky <rsergievsky@yandex.ru>
2019-02-07 19:35:13 +0300

    deploy at 1549557313

--- modules/game.js
+++ modules/game.js
@@ -70,7 +70,7 @@ module.exports = {
     }
   },
   startGame: async function() {
-    const count = await db.query(`SELECT COUNT(*) as FROM games WHERE win=1`);
+    const count = await db.query(`SELECT COUNT(*) as count FROM games WHERE win=1`);
     console.log(count);
     const [game] = await db.query(`SELECT * FROM games ORDER BY id DESC LIMIT 1`);
     if (game == null || game.win) {

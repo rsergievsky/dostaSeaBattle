81efa659f2dd96d9881511252ed6b13cfd2fb839
rsergievsky <rsergievsky@yandex.ru>
2019-02-08 14:31:37 +0300

    deploy at 1549625497

--- modules/vk-api.js
+++ modules/vk-api.js
@@ -33,7 +33,10 @@ module.exports = {
     try {
 
       const [row] = await db.query(`SELECT * FROM answers WHERE id=${answer.id}`);
-      if (!row) return false;
+      if (!row) {
+        delete env.busyTokens[answer.token_index];
+        return false;
+      }
 
       const res = JSON.parse(await rp.get(`https://api.vk.com/method/wall.createComment?owner_id=${-env.groupID}&post_id=${env.postID}&message=${encodeURIComponent(answer.message)}&from_group=${env.groupID}&attachments=${answer.attachments}&reply_to_comment=${answer.comment_id}&access_token=${token}${captcha}&v=5.92`));
       if (res.error == null || res.error.error_code == '100') {
